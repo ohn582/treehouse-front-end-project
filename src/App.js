d3bdaa060cad700b6c5b91d8124122eec807d5f2
Neal <no4259@gmail.com>
2020-10-27 23:15:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,30 +11,32 @@ class App extends Component {
       <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <a className="navbar-brand" href="#">Content Former Logo</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
 
-          <div className="collapse-navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse-navbar-collapse" className="nav" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">How It Works</a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Contact Us</a>
+              </li>
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   V
                 </a>
 
-
-                {/* <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                {/* <div className="dropdown-menu">
                   <form class="px-4 py-3">
                     <div class="form-group">
                       <label for="exampleDropdownFormEmail1">Email address</label>
-                      <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
+                      <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com"/>
                     </div>
                     <div class="form-group">
                       <label for="exampleDropdownFormPassword1">Password</label>
-                      <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+                      <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password"/>
                     </div>
                     <div class="form-check">
-                      <input type="checkbox" class="form-check-input" id="dropdownCheck">
+                      <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
                         <label class="form-check-label" for="dropdownCheck">
                           Remember me
                         </label>
@@ -45,6 +47,17 @@ class App extends Component {
                   <a class="dropdown-item" href="#">New around here? Sign up</a>
                   <a class="dropdown-item" href="#">Forgot password?</a>
                 </div> */}
+
+                <div className="dropdown-menu">
+                  
+                  <div class="dropdown-divider"></div>
+                    <label for="exampleDropdownFormEmail1">Switch Supplier</label>
+                    <br/>
+                    <label for="exampleDropdownFormPassword1">Settings</label>
+                    <br />
+                    <label for="exampleDropdownFormPassword1">Log In</label>
+                  
+                </div>
                 
               </li>
             </ul>
@@ -53,7 +66,6 @@ class App extends Component {
         <Router>
           <div>
             <h1>Welcome to Game App</h1>
-            <Link to="/home" className='home'>Home</Link>
           </div>
         </Router>
       </>
